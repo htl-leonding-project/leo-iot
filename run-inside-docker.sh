9936e352fed290e20ab19c687c293ef5a88a46fc
Quirin <quirin.ecker@bajupa.com>
2021-01-07 13:34:35 +0100

    fixed cos

--- run-inside-docker.sh
+++ run-inside-docker.sh
@@ -4,9 +4,9 @@ echo "BUILD_DIR->${BUILD_DIR}"
 OLD=${PWD}
 echo "OLD->${OLD}"
 docker run --rm \
-           -v ${PWD}/../:/documents \
+           -v ${PWD}:/documents \
            --env-file ./.env \
-           asciidoctor/docker-asciidoctor:1.2.0  /bin/bash -c "/documents/documentation/publish-gh-pages-inside-docker.sh"
+           asciidoctor/docker-asciidoctor:1.2.0  /bin/bash -c "/documents/publish-gh-pages-inside-docker.sh"
            #asciidoctor/docker-asciidoctor:1.2.0  /bin/bash -c "/documents/build-html-docker-inside-docker.sh"
 cd $PWD/$BUILD_DIR
 CURRENT_FOLDER=${PWD}
