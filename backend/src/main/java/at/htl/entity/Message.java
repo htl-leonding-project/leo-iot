7ce58705fb5c77db45dd8992851e58b86f91f3e4
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-02-26 13:08:20 +0100

    added Message entity

--- backend/src/main/java/at/htl/entity/Message.java
+++ backend/src/main/java/at/htl/entity/Message.java
@@ -15,7 +15,70 @@ public class Message implements Serializable {
     private Language languageId;
     @Id
     private String messageName;
+    private String level;
+    private String title;
+    private String descritption;
 
-    
+    public Message() {
+    }
+
+    public Message(int messageId, Language languageId, String messageName, String level, String title, String descritption) {
+        this.messageId = messageId;
+        this.languageId = languageId;
+        this.messageName = messageName;
+        this.level = level;
+        this.title = title;
+        this.descritption = descritption;
+    }
+
+    // region Getter and Setter
+    public int getMessageId() {
+        return messageId;
+    }
+
+    public void setMessageId(int messageId) {
+        this.messageId = messageId;
+    }
+
+    public Language getLanguageId() {
+        return languageId;
+    }
+
+    public void setLanguageId(Language languageId) {
+        this.languageId = languageId;
+    }
+
+    public String getMessageName() {
+        return messageName;
+    }
+
+    public void setMessageName(String messageName) {
+        this.messageName = messageName;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescritption() {
+        return descritption;
+    }
+
+    public void setDescritption(String descritption) {
+        this.descritption = descritption;
+    }
+    // endregion
 }
 
