93c41e4a0ab3688463d03f11d7c074d670ece254
Quirin <quirin.ecker@bajupa.com>
2021-02-26 13:55:34 +0100

    extrected configurations into config files

--- backend/src/main/java/at/htl/brokerclient/CamelPahoReceiver.java
+++ backend/src/main/java/at/htl/brokerclient/CamelPahoReceiver.java
@@ -3,6 +3,7 @@ package at.htl.brokerclient;
 import at.htl.controller.MeasurementRepository;
 import at.htl.entity.Measurement;
 import io.quarkus.runtime.StartupEvent;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
@@ -12,26 +13,43 @@ import javax.json.JsonObject;
 import javax.json.bind.JsonbBuilder;
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.TimeZone;
 
 public class CamelPahoReceiver implements MqttCallback {
     @Inject
     MeasurementRepository measurementRepository;
 
+    @ConfigProperty(name = "mqtt.url")
+    String mqttUrl;
+
+    @ConfigProperty(name = "mqtt.topic")
+    String mqttTopic;
+
+    @ConfigProperty(name = "mqtt.username")
+    String mqttUsername;
+
+    @ConfigProperty(name = "MQTT_PASSWORD")
+    Optional<String> mqttPassword;
+
+    @ConfigProperty(name = "mqtt.qos", defaultValue = "1")
+    int mqttQos;
+
     void onStart(@Observes StartupEvent ev) {
         try {
             MqttAsyncClient client = new MqttAsyncClient(
-                    "tcp://vm90.htl-leonding.ac.at:1883",
-                    "iot-server",
+                    mqttUrl,
+                    MqttAsyncClient.generateClientId(),
                     new MemoryPersistence()
             );
 
             MqttConnectOptions connOpts = new MqttConnectOptions();
             connOpts.setCleanSession(true);
-            connOpts.setUserName("student");
-            connOpts.setPassword("passme".toCharArray());
+            connOpts.setUserName(mqttUsername);
+
+            connOpts.setPassword(mqttPassword.orElseThrow().toCharArray());
             client.connect(connOpts).waitForCompletion();
-            client.subscribe("og/+/+/test", 1);
+            client.subscribe(mqttTopic, mqttQos);
             client.setCallback(this);
 
         } catch (MqttException e) {
@@ -41,12 +59,10 @@ public class CamelPahoReceiver implements MqttCallback {
     }
 
     @Override
-    public void connectionLost(Throwable throwable) {
-
-    }
+    public void connectionLost(Throwable throwable) { }
 
     @Override
-    public void messageArrived(String topic, MqttMessage msg) throws Exception {
+    public void messageArrived(String topic, MqttMessage msg) {
         JsonObject jsonObject = JsonbBuilder.create().fromJson(new String(msg.getPayload()), JsonObject.class);
 
         Measurement measurement = new Measurement(
@@ -63,7 +79,5 @@ public class CamelPahoReceiver implements MqttCallback {
     }
 
     @Override
-    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
-
-    }
+    public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) { }
 }
