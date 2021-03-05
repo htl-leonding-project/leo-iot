c237f979ea2eba015dc42855142da5078d77d364
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-03-05 11:49:20 +0100

    #50 added updated Measurement entitty

---
+++ backend/src/main/java/at/htl/entity/Measurement.java
@@ -0,0 +1,27 @@
+package at.htl.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+@Entity
+public class Measurement implements Serializable {
+
+    @Id
+    private Timestamp time;
+    @Id
+    @ManyToOne
+    private Sensor sensorId;
+    private Float measurement;
+
+    public Measurement() {
+    }
+
+    public Measurement(Timestamp time, Sensor sensorId, Float measurement) {
+        this.time = time;
+        this.sensorId = sensorId;
+        this.measurement = measurement;
+    }
+}
