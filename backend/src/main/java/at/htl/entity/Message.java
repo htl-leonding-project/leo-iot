fec8745cdc18b50af207297bcd29f8e978ba5496
Quirin <quirin.ecker@bajupa.com>
2021-03-12 14:08:22 +0100

    added Functions to remove warnings

--- backend/src/main/java/at/htl/entity/Message.java
+++ backend/src/main/java/at/htl/entity/Message.java
@@ -38,6 +38,20 @@ public class Message{
 
         }
 
+        @Override
+        public int hashCode() {
+            return super.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return super.equals(obj);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString();
+        }
     }
 
     @EmbeddedId
