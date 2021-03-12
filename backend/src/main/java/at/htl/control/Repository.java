e948352d4e9a1f4d5c5edcf9caa97657ff4e60be
Quirin <quirin.ecker@bajupa.com>
2021-03-12 09:13:17 +0100

    implemented Repository base class

---
+++ backend/src/main/java/at/htl/control/Repository.java
@@ -0,0 +1,23 @@
+package at.htl.control;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepositoryBase;
+
+import javax.transaction.Transactional;
+
+public abstract class Repository<Entity, Identification> implements PanacheRepositoryBase<Entity, Identification> {
+
+    @Transactional
+    public Entity save(Entity entity) {
+        return getEntityManager().merge(entity);
+    }
+
+    @Transactional
+    public boolean removeById(Identification id) {
+        return deleteById(id);
+    }
+
+    @Transactional
+    public void remove(Entity entity) {
+        delete(entity);
+    }
+}
