59e733787ba75ecad5ab73d4e268b16c88cd610a
Quirin <quirin.ecker@bajupa.com>
2021-04-19 21:42:08 +0200

    implemented Method for Measurement Repository for getting Values with from timestamp and to timestamp

--- backend/src/main/java/at/htl/repository/MeasurementRepository.java
+++ backend/src/main/java/at/htl/repository/MeasurementRepository.java
@@ -1,9 +1,48 @@
 package at.htl.repository;
 
 import at.htl.entity.Measurement;
+import at.htl.entity.Sensor;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.List;
 
 @ApplicationScoped
 public class MeasurementRepository extends Repository<Measurement, Measurement.MeasurementKey> {
+
+    public List<Measurement> get(Timestamp from, Timestamp to, Sensor sensor) throws IllegalArgumentException {
+        if (from.after(to)) throw new IllegalArgumentException();
+        var query = getEntityManager().createQuery(
+                "select m " +
+                        "from Measurement m " +
+                        "where m.measurementKey.timestamp >= :fromTimestamp " +
+                        "and m.measurementKey.timestamp <= :toTimestamp " +
+                        "and m.measurementKey.sensor = :sensor",
+                Measurement.class
+        );
+
+        query.setParameter("fromTimestamp", from);
+        query.setParameter("toTimestamp", to);
+        query.setParameter("sensor", sensor);
+
+        return query.getResultList();
+    }
+
+    public List<Measurement> get(Timestamp from, Timestamp to) throws IllegalArgumentException {
+        if (from.after(to)) throw new IllegalArgumentException();
+        var query = getEntityManager().createQuery(
+                "select m " +
+                        "from Measurement m " +
+                        "where m.measurementKey.timestamp >= :fromTimestamp " +
+                        "and m.measurementKey.timestamp <= :toTimestamp",
+                Measurement.class
+        );
+
+        query.setParameter("fromTimestamp", from);
+        query.setParameter("toTimestamp", to);
+
+        return query.getResultList();
+    }
+
 }
