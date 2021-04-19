23e78b37b5bc484185ef3d6201716b8a3b4c185a
VinzentKronreif <vini.kro@gmail.com>
2021-04-19 14:56:54 +0200

    sends correct values, but not as topic only all in one message

--- new-value-simulator/src/main/java/at/htl/mqtt/client/boundary/MyValueGenerator.java
+++ new-value-simulator/src/main/java/at/htl/mqtt/client/boundary/MyValueGenerator.java
@@ -10,9 +10,11 @@ import org.eclipse.microprofile.reactive.messaging.Emitter;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
+import org.json.JSONObject;
 
 /**
  * https://stackoverflow.com/questions/62883516/publish-subscribe-mqtt-using-smallrye-reactive-messaging-dynamically
@@ -23,9 +25,7 @@ public class MyValueGenerator {
 
     @Inject
     @Channel("topic-values")
-    Emitter<Double> emitter;
-
-    Random rnd = new Random();
+    Emitter<String> emitter;
 
     void init(@Observes StartupEvent event) {
 
@@ -34,9 +34,20 @@ public class MyValueGenerator {
 
             Observable.interval(0, 5, TimeUnit.SECONDS)
                     .subscribe(value -> {
-                        double payload = rnd.nextDouble();
-                        emitter.send(MqttMessage.of(topic, payload));
-                        System.out.println("Sending value -> " + payload);
+                        Map values = new HashMap<String, Object>();
+                        values.put("temp", System.currentTimeMillis());
+                        values.put("noise", Math.random() * 100);
+                        values.put("trafficlight", Math.floor(Math.random()));
+                        values.put("temperature", Math.random() * 20);
+                        values.put("humidity", Math.random() * 20);
+                        values.put("pressure", Math.random() * 1000);
+                        values.put("luminosity", Math.random() * 2000);
+                        values.put("co2", Math.random() * 700);
+                        values.put("motion", Math.floor(Math.random()));
+
+                        JSONObject jsonValue = new JSONObject(values);
+                        emitter.send(MqttMessage.of(topic, jsonValue.toString()));
+                        System.out.println("Sending value -> " + jsonValue);
                     });
         }
     }
