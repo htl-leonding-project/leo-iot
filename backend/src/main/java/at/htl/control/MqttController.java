ac3dfd504557884f8315f0d8950c6e3b67993c7b
Quirin <quirin.ecker@bajupa.com>
2021-05-19 07:46:24 +0200

    changed mqtt controller implementation because of raise condition

--- backend/src/main/java/at/htl/control/MqttController.java
+++ backend/src/main/java/at/htl/control/MqttController.java
@@ -3,7 +3,6 @@ package at.htl.control;
 import at.htl.config.MqttConfiguration;
 import at.htl.util.MqttSubscribe;
 import io.quarkus.runtime.ShutdownEvent;
-import io.quarkus.runtime.StartupEvent;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
@@ -12,10 +11,7 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 @ApplicationScoped
 public class MqttController {
@@ -23,44 +19,47 @@ public class MqttController {
     @Inject
     MqttConfiguration configuration;
 
-    private Future<MqttClient> mqttClient;
+    private MqttClient mqttClient;
 
     private final ExecutorService executor = Executors.newSingleThreadExecutor();
 
-    void init(@Observes StartupEvent ev) {
-        mqttClient = executor.submit(() -> {
+    void init() throws MqttException {
+        mqttClient =  new MqttClient(
+                configuration.getFullUrl(),
+                MqttClient.generateClientId(),
+                new MemoryPersistence()
+        );
+
+        MqttConnectOptions options = new MqttConnectOptions();
+
+        if (configuration.password.isPresent() && configuration.username.isPresent()) {
+            options.setUserName(configuration.username.get());
+            options.setPassword(configuration.password.get().toCharArray());
+        }
+
+        options.setCleanSession(true);
+
+        mqttClient.connect(options);
+    }
+
+    private MqttClient getClient() {
+        if (mqttClient == null) {
             try {
-                var client =  new MqttClient(
-                        configuration.getFullUrl(),
-                        MqttClient.generateClientId(),
-                        new MemoryPersistence()
-                );
-
-                MqttConnectOptions options = new MqttConnectOptions();
-
-                if (configuration.password.isPresent() && configuration.username.isPresent()) {
-                    options.setUserName(configuration.username.get());
-                    options.setPassword(configuration.password.get().toCharArray());
-                }
-
-                options.setCleanSession(true);
-
-                client.connect(options);
-
-                return client;
+                init();
             } catch (MqttException e) {
                 e.printStackTrace();
-                return null;
             }
-        });
+        }
+
+        return mqttClient;
     }
 
-    void cleanUp(@Observes ShutdownEvent ev) throws MqttException, ExecutionException, InterruptedException {
-        mqttClient.get().disconnect();
+    void cleanUp(@Observes ShutdownEvent ev) throws MqttException {
+        getClient().disconnect();
     }
 
-    public void subscribe(String topic, MqttSubscribe mqttSubscribe) throws ExecutionException, InterruptedException, MqttException {
-        mqttClient.get().subscribeWithResponse(topic, ((s, mqttMessage) -> mqttSubscribe.subscribe(topic, mqttMessage)));
+    public void subscribe(String topicFilter, MqttSubscribe mqttSubscribe) throws MqttException {
+        getClient().subscribeWithResponse(topicFilter, mqttSubscribe::subscribe);
     }
 
 }
