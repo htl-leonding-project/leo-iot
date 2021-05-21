1529b1b42789ad180f0bcf93201d26fe17e88b7c
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-05-21 19:41:17 +0200

    added getSensorType endpoint

--- backend/src/main/java/at/htl/boundary/SensorTypeResource.java
+++ backend/src/main/java/at/htl/boundary/SensorTypeResource.java
@@ -1,4 +1,30 @@
 package at.htl.boundary;
 
+import at.htl.repository.SensorTypeRepository;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+@Path("sensortype")
 public class SensorTypeResource {
+
+    @Inject
+    SensorTypeRepository sensorTypeRepository;
+
+    @GET
+    public Response getSensorType(@QueryParam("id") Long sensorTypeId){
+        if (sensorTypeId != null){
+            return Response
+                    .accepted(sensorTypeRepository.findById(sensorTypeId))
+                    .build();
+        }else {
+            return Response
+                    .accepted(sensorTypeRepository.findAll())
+                    .build();
+        }
+    }
+
 }
