cc616fcca325854b29236f288f53c631c8c11ea6
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-05-21 20:37:00 +0200

    added getThing endpoint

--- backend/src/main/java/at/htl/boundary/ThingResource.java
+++ backend/src/main/java/at/htl/boundary/ThingResource.java
@@ -1,4 +1,30 @@
 package at.htl.boundary;
 
+import at.htl.repository.ThingRepository;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+@Path("")
 public class ThingResource {
+
+    @Inject
+    ThingRepository thingRepository;
+
+    @GET
+    public Response getThing(@QueryParam("id") Long thingId){
+        if(thingId != null) {
+            return Response
+                    .accepted(thingRepository.findById(thingId))
+                    .build();
+        }else {
+            return  Response
+                    .accepted(thingRepository.findAll())
+                    .build();
+        }
+    }
+
 }
