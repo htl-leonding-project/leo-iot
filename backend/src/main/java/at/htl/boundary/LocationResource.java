ca0c073d0b277f014ab9c9e1ce13ccfbc56730a8
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-05-21 19:16:57 +0200

    added getLocation endpoint

--- backend/src/main/java/at/htl/boundary/LocationResource.java
+++ backend/src/main/java/at/htl/boundary/LocationResource.java
@@ -1,7 +1,30 @@
 package at.htl.boundary;
 
+import at.htl.repository.LocationRepository;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
 
 @Path("location")
 public class LocationResource {
+
+    @Inject
+    LocationRepository locationRepository;
+
+    @GET
+    public Response getLocation(@QueryParam("id") Long locationId){
+        if (locationId != null){
+            return Response
+                    .accepted(locationRepository.findById(locationId))
+                    .build();
+        }else{
+            return Response
+                    .accepted(locationRepository.findAll())
+                    .build();
+        }
+    }
+
 }
