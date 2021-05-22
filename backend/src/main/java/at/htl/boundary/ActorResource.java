beafc89fd5c32751a929b18d17c0143e8dab418c
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-05-22 13:04:31 +0200

    added getActor endpoint

--- backend/src/main/java/at/htl/boundary/ActorResource.java
+++ backend/src/main/java/at/htl/boundary/ActorResource.java
@@ -1,4 +1,30 @@
 package at.htl.boundary;
 
+import at.htl.repository.ActorRepository;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+@Path("actor")
 public class ActorResource {
+
+    @Inject
+    ActorRepository actorRepository;
+
+    @GET
+    public Response getActor(@QueryParam("id") Long actorType){
+        if(actorType != null){
+            return Response
+                    .accepted(actorRepository.findById(actorType))
+                    .build();
+        }else {
+            return Response
+                    .accepted(actorRepository.findAll())
+                    .build();
+        }
+    }
+
 }
