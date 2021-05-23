8c05a58040d4615977075774f022afcbd15c58be
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-05-23 16:26:11 +0200

    created ActorActionResource.java

--- backend/src/main/java/at/htl/boundary/ActorActionResource.java
+++ backend/src/main/java/at/htl/boundary/ActorActionResource.java
@@ -1,4 +1,27 @@
 package at.htl.boundary;
 
+import at.htl.repository.ActorActionRepository;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+@Path("actoraction")
 public class ActorActionResource {
+
+    @Inject
+    ActorActionRepository actorActionRepository;
+
+    @GET
+    public Response getActorAction(@QueryParam("id") Long actorActionId){
+        if (actorActionId != null){
+            return  Response.accepted().build();
+        }else{
+            return Response
+                    .accepted(actorActionRepository.findAll())
+                    .build();
+        }
+    }
 }
