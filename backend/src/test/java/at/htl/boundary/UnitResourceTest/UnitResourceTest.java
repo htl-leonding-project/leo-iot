32ffa669d0c30c3514f542e54f622890d5736aa6
VinzentKronreif <vini.kro@gmail.com>
2021-06-06 23:21:56 +0200

    created UnitResourceTest package

--- backend/src/test/java/at/htl/boundary/UnitResourceTest/UnitResourceTest.java
+++ backend/src/test/java/at/htl/boundary/UnitResourceTest/UnitResourceTest.java
@@ -4,5 +4,8 @@ import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
 public class UnitResourceTest {
-
+    @Karate.Test
+    Karate testCreateTemplateApplicationAsStudent() {
+        return Karate.run("template-application-creation").relativeTo(getClass());
+    }
 }
