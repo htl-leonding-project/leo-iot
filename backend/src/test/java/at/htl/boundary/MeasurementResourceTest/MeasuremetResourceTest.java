6f2029780fb4be8ff8843ebb9541ce6217adf583
rieserfelix <dafelix.rieser@gmail.com>
2021-06-07 23:47:16 +0200

    create measurement-create.feature

--- backend/src/test/java/at/htl/boundary/MeasurementResourceTest/MeasuremetResourceTest.java
+++ backend/src/test/java/at/htl/boundary/MeasurementResourceTest/MeasuremetResourceTest.java
@@ -1,4 +1,10 @@
 package at.htl.boundary.MeasurementResourceTest;
 
+import com.intuit.karate.junit5.Karate;
+
 public class MeasuremetResourceTest {
+
+    @Karate.Test
+    Karate testCreateMeasurement() { return Karate.run("measurement-create.feature").relativeTo(getClass()); }
+
 }
