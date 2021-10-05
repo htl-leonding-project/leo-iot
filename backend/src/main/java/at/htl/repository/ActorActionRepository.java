42e14d84dd5f9e98dfd9c9fbcd3ff5fea183f05c
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-10-05 10:23:07 +0200

    added getByTimestamp method

--- backend/src/main/java/at/htl/repository/ActorActionRepository.java
+++ backend/src/main/java/at/htl/repository/ActorActionRepository.java
@@ -3,7 +3,20 @@ package at.htl.repository;
 import at.htl.entity.ActorAction;
 
 import javax.enterprise.context.ApplicationScoped;
+import java.sql.Timestamp;
+import java.util.List;
 
 @ApplicationScoped
 public class ActorActionRepository extends Repository<ActorAction, ActorAction.ActorActionKey> {
+
+    public List<ActorAction> getActorActionByTimestamp(Timestamp timestamp){
+        var query = getEntityManager().createQuery("select a " +
+                "from ActorAction a " +
+                "where a.actorActionKey.timestamp = :searchedTimestamp",
+                ActorAction.class);
+
+        query.setParameter("searchedTimestamp", timestamp);
+        return query.getResultList();
+    }
+
 }
