d24118ad67c29ce69dc9093828bbf92272f07648
PhilippEdlinger <p.edlinger@students.htl-leonding.ac.at>
2021-10-22 10:13:11 +0200

    setup actoraction for openapi

--- backend/src/main/java/at/htl/boundary/SensorTypeResource.java
+++ backend/src/main/java/at/htl/boundary/SensorTypeResource.java
@@ -3,6 +3,9 @@ package at.htl.boundary;
 import at.htl.entity.Location;
 import at.htl.entity.SensorType;
 import at.htl.repository.SensorTypeRepository;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -10,12 +13,17 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("sensortype")
+@Tag(name= "Sensortype endpoint")
 public class SensorTypeResource {
 
     @Inject
     SensorTypeRepository sensorTypeRepository;
 
     @GET
+    @Operation(
+            summary = "get a SensorTYpe",
+            description ="get the desired sensortype by id"
+    )
     public Response getSensorType(@QueryParam("id") Long sensorTypeId){
         if (sensorTypeId != null){
             return Response
@@ -30,6 +38,10 @@ public class SensorTypeResource {
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
+    @Operation (
+            summary = "save a sensortype",
+            description = "save the desired sensortype"
+    )
     public Response addSensorType(SensorType sensorType){
         return Response
                 .accepted(sensorTypeRepository.save(sensorType))
@@ -37,6 +49,11 @@ public class SensorTypeResource {
     }
 
     @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(
+            summary = "delete a actor",
+            description = "delete the desired sensorType by id"
+    )
     public Response deleteSensorTypeById(@QueryParam("id") Long sensorTypeId){
         return Response
                 .accepted(sensorTypeRepository.deleteById(sensorTypeId))
