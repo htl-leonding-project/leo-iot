e360c848fab9a2823b892b0ac0f71d0ce6d8d810
VinzentKronreif <vini.kro@gmail.com>
2021-11-16 11:46:28 +0100

    #leoiot-13 added hibernate for database

--- new-value-simulator/src/main/java/at/htl/mqtt/client/entity/Room.java
+++ new-value-simulator/src/main/java/at/htl/mqtt/client/entity/Room.java
@@ -1,15 +1,39 @@
 package at.htl.mqtt.client.entity;
 
-public class Room {
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
+import javax.json.bind.annotation.JsonbProperty;
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Entity
+@XmlRootElement
+@Schema(description = "This is a Room of the HTL")
+@Table(name = "rooms")
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonbProperty("room_name")
+    @Column(name = "room_name")
+    @Schema(required = true)
     private String name;
 
     public Room(String name) {
         this.name = name;
     }
 
+    public Room() {
+
+    }
+
     public String getName() {
         return name;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public void setName(String name) {
